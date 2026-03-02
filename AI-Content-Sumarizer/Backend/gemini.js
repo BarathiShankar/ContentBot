03d2c91bd73d0cc7413e1443400b0b3fe385b4e1
BarathiShankar <barathishankarr@gmail.com>
2026-03-02 07:57:39 +0530

    Completed the modeling of gemini,need to add other front end

--- AI-Content-Sumarizer/Backend/gemini.js
+++ AI-Content-Sumarizer/Backend/gemini.js
@@ -1,8 +1,12 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-
+let count=0;
 export async function summarizeText(inputText) {
+  if(count>5)
+  {
+    //nav to login page
+  }
   const prompt = `
 Summarize the following text in bullet points only.
 Do not include any introductory phrases like "Here’s a summary".
@@ -11,9 +15,9 @@ Do not include any introductory phrases like "Here’s a summary".
 
 
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
     const result = await model.generateContent(prompt);
-
+    count++;
     // ✅ Correct way to extract text
     return result.response.text();
   } catch (error) {
